cd35bd24f0f105711ef79e2abd78ccff3d6977f5
alakhber <nakhiyev.alakhber@gmail.com>
2022-07-12 01:24:11 +0400

    bond model done

--- app/Models/Bond.php
+++ app/Models/Bond.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Bond extends Model
 {
     use HasFactory;
+    
+    protected $fillable  = [
+        'emission_date',
+        'turnover_end_date',
+        'nominal',
+        'coupon_redemption_frequency',
+        'interest_calculation_period',
+        'coupon_interest'
+    ];
 }
