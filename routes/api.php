d8fb6886b162efeaf170da2b29cbe74fe0f394bd
alakhber <nakhiyev.alakhber@gmail.com>
2022-07-12 01:43:44 +0400

    purchaseOrderStore route added

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('bond')->controller(BondController::class)->group(function () {
     Route::get('{id}/payouts', 'payouts');
+    Route::post('{id}/order', 'purchaseOrderStore');
 });
