1a56f68549d0ef408065f4f1bd96f7964f707952
alakhber <nakhiyev.alakhber@gmail.com>
2022-07-12 01:57:04 +0400

    bondInterestPayments route added

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('bond')->controller(BondController::class)->group(function () {
     Route::get('{id}/payouts', 'payouts');
     Route::post('{id}/order', 'purchaseOrderStore');
+    Route::post('order/{id}', 'bondInterestPayments');
 });
