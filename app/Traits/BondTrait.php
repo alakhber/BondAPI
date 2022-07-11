d31f96b5923b34e716342b93c3f3f9382af0f2bf
alakhber <nakhiyev.alakhber@gmail.com>
2022-07-12 02:56:03 +0400

    bondPaymentDates method done

--- app/Traits/BondTrait.php
+++ app/Traits/BondTrait.php
@@ -9,6 +9,34 @@ trait BondTrait
 {
     public function bondPaymentDates(Bond $bond)
     {
+        switch ($bond->interest_calculation_period) {
+            case 360:
+                $periodDurationWithDay = 12 / $bond->coupon_redemption_frequency * 30;
+                break;
+            case 364:
+                $periodDurationWithDay = 364 / $bond->coupon_redemption_frequency;
+                break;
+            case 365:
+                $periodDurationWithDay = 12 / $bond->coupon_redemption_frequency;
+                break;
+        }
+
+        $emissionDateWithAddDays = _parseDate($bond->emission_date)->addDays($periodDurationWithDay);
+        $emiisonDate = _parseDate($bond->emission_date);
+
+        $calcWithOutStrdayAndSnday = collect($emiisonDate->monthsUntil($emissionDateWithAddDays))->filter(function ($date) {
+            if (_parseDate($date)->isSaturday()) {
+                $date->addDays(2);
+            } elseif (_parseDate($date)->isSunday()) {
+                $date->addDays(1);
+            }
+
+            return $date;
+        })->map(function ($date) {
+            return ['date' => $date->format('Y-m-d')];
+        });
+
+        return $calcWithOutStrdayAndSnday;
     }
 
     public function interestPaymentsWithAmount(PurchaseOrder $purchaseOrder, $bondPaymentDates)
