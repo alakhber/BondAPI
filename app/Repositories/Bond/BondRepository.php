da67dc8ff8ad4d769649320088eb1fbb080037a7
alakhber <nakhiyev.alakhber@gmail.com>
2022-07-12 02:35:17 +0400

    payouts method done

--- app/Repositories/Bond/BondRepository.php
+++ app/Repositories/Bond/BondRepository.php
@@ -2,12 +2,43 @@
 namespace App\Repositories\Bond;
 
 use App\Http\Requests\PurchaseOrderStoreRequest;
+use App\Http\Resources\PaymentDatesResource;
+use App\Models\Bond;
+use Carbon\Carbon;
 
 class BondRepository implements BondInterface
 {
     public function payouts(int $id)
     {
-        
+        $bond = Bond::findOrFail($id);
+        switch ($bond->interest_calculation_period) {
+            case 360:
+                $periodDurationWithDay = 12 / $bond->coupon_redemption_frequency * 30;
+                break;
+            case 364:
+                $periodDurationWithDay = 364 / $bond->coupon_redemption_frequency;
+                break;
+            case 365:
+                $periodDurationWithDay = 12 / $bond->coupon_redemption_frequency;
+                break;
+        }
+
+        $emissionDateWithAddDays = Carbon::parse($bond->emission_date)->addDays($periodDurationWithDay);
+        $emiisonDate = Carbon::parse($bond->emission_date);
+
+        $calcWithOutStrdayAndSnday = collect($emiisonDate->monthsUntil($emissionDateWithAddDays))->filter(function ($date) {
+            if (Carbon::parse($date)->isSaturday()) {
+                $date->addDays(2);
+            } elseif (Carbon::parse($date)->isSunday()) {
+                $date->addDays(1);
+            }
+
+            return $date;
+        })->map(function ($date) {
+            return ['date' => $date->format('Y-m-d')];
+        });
+
+        return PaymentDatesResource::collection($calcWithOutStrdayAndSnday);
     }
 
     public function purchaseOrderStore(PurchaseOrderStoreRequest $request, int $id)
