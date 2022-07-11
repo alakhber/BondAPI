4f913af68a49a98cb13f3e1132b71fa189337d3d
alakhber <nakhiyev.alakhber@gmail.com>
2022-07-12 01:37:16 +0400

    PaymentDatesResource done

--- app/Http/Resources/PaymentDatesResource.php
+++ app/Http/Resources/PaymentDatesResource.php
@@ -6,14 +6,17 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class PaymentDatesResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
+    public function __construct($resource)
+    {
+        self::$wrap = 'dates';
+        
+        $this->resource = $resource;
+    }
+
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'date' => $this['date'],
+        ];
     }
 }
