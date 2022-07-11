92f9b3500959d2e2dbe6020911ebc2cb3420821c
alakhber <nakhiyev.alakhber@gmail.com>
2022-07-12 02:49:59 +0400

    interestPaymentsWithAmount method added

--- app/Traits/BondTrait.php
+++ app/Traits/BondTrait.php
@@ -3,11 +3,15 @@
 namespace App\Traits;
 
 use App\Models\Bond;
+use App\Models\PurchaseOrder;
 
 trait BondTrait
 {
     public function bondPaymentDates(Bond $bond)
     {
-        
+    }
+
+    public function interestPaymentsWithAmount(PurchaseOrder $purchaseOrder, $bondPaymentDates)
+    {
     }
 }
