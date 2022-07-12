37853265c6d1ba06c179dacef041c6ad4cfdd197
alakhber <nakhiyev.alakhber@gmail.com>
2022-07-12 05:15:09 +0400

    error fixes

--- database/migrations/2022_07_11_211605_create_bonds_table.php
+++ database/migrations/2022_07_11_211605_create_bonds_table.php
@@ -14,16 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('bonds', function (Blueprint $table) {
-            Schema::create('bonds', function (Blueprint $table) {
                 $table->id();
                 $table->date('emission_date');
                 $table->date('turnover_end_date');
                 $table->unsignedBigInteger('nominal');
-                $table->enum('coupon_redemption_frequency', [1, 2, 4, 12]);
-                $table->enum('interest_calculation_period', [360, 364, 365]);
+                $table->enum('coupon_redemption_frequency', [1, 2, 4,12]);
+                $table->enum('interest_calculation_period', [360, 364,365]);
                 $table->unsignedtinyinteger('coupon_interest');
                 $table->timestamps();
-            });
         });
     }
 
