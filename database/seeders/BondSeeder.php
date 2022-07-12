d3932b2c782ab9f4394eaab2d69efedb2bb333f7
alakhber <nakhiyev.alakhber@gmail.com>
2022-07-12 05:14:42 +0400

    seeders done

--- database/seeders/BondSeeder.php
+++ database/seeders/BondSeeder.php
@@ -2,18 +2,39 @@
 
 namespace Database\Seeders;
 
+use App\Models\Bond;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class BondSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+   
     public function run()
     {
-        //
+        Bond::create([
+            'emission_date'=>'2021-11-08',
+            'turnover_end_date'=>'2022-11-03',
+            'nominal'=>100,
+            'coupon_redemption_frequency'=>'4',
+            'interest_calculation_period'=>'360',
+            'coupon_interest'=>10
+        ]);
+
+        Bond::create([
+            'emission_date'=>'2021-11-08',
+            'turnover_end_date'=>'2022-11-07',
+            'nominal'=>200,
+            'coupon_redemption_frequency'=>'4',
+            'interest_calculation_period'=>'364',
+            'coupon_interest'=>20
+        ]);
+        Bond::create([
+            'emission_date'=>'2021-11-08',
+            'turnover_end_date'=>'2022-11-08',
+            'nominal'=>300,
+            'coupon_redemption_frequency'=>'4',
+            'interest_calculation_period'=>'365',
+            'coupon_interest'=>30
+        ]);
     }
 }
--- database/seeders/PurchaseOrderSeeder.php
+++ database/seeders/PurchaseOrderSeeder.php
@@ -2,18 +2,19 @@
 
 namespace Database\Seeders;
 
+use App\Models\PurchaseOrder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PurchaseOrderSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    
     public function run()
     {
-        //
+        PurchaseOrder::create([
+            'bond_id'=>1,
+            'order_date'=>'2021-11-23',
+            'bond_received'=>10,
+        ]);
     }
 }
